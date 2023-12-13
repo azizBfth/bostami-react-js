2f3ff58240c2cfa765f785b1320119a062a76917
azizBfth <boufath.aziz83@gmail.com>
2023-12-13 15:09:55 +0100

    first commit

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import About from './About'
+import MainMenu from './MainMenu'
+import Resume from './Resume'
+import Work from './Work'
+import Blog from './Blog'
+import Contact from './Contact'
+
+const MainContent = () => {
+  return (
+    <div className='col-span-12 lg:col-span-8 rounded-2xl'>
+      <MainMenu></MainMenu>
+      <About ></About>
+      <Resume></Resume>
+      <Work> </Work>
+      <Blog></Blog>
+      <Contact></Contact>
+    </div>
+  )
+}
+
+export default MainContent
